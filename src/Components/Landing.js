08391d911ec1c73e9d8ea5cf8f9105613a969923
MohammadAli <galaxy.mrz8@gmail.com>
2023-04-10 21:17:32 +0430

    searchCoins to filter Currency

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { getCoin } from '../Services/api';
 import Loader from './Loader';
+import Coin from './Coin';
 
 const Landing = () => {
     const [coins, setCoins] = useState([])
+    const [search, setSearch] = useState("")
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -13,13 +15,18 @@ const Landing = () => {
         }
         fetchAPI()
     }, [])
+
+    const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
     return (
         <>
-            <input type="text" placeholder='search' />
+            <input type="text" placeholder='search' value={search} onChange={(e) => setSearch(e.target.value)} />
             {
                 coins.length ? <div>
                     {
-                        coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                        searchCoins.map(coin => <Coin
+                            key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} price={coin.current_price} marketCap={coin.market_cap}
+                            priceChange={coin.market_cap_change_percentage_24h}
+                        />)
                     }
                 </div> :
                     <Loader />
